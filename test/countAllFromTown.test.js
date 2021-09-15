810ac041d056fe3b2542593035683ef8a06f177b
Thabile Dube <taniatdube1@gmail.com>
2021-09-15 21:03:48 +0200

    tests done

--- test/countAllFromTown.test.js
+++ test/countAllFromTown.test.js
@@ -4,10 +4,13 @@ let countAllFromTown =require("../function/countAllFromTown");
 describe('countAllFromTown' , function(){
 
   it('should return "number registration numbers in the string " when it is equal 3' , function(){
-        assert.equal(fromStellies, )
-        
-       var fromStellies = countAllFromTown('CL 124,CY 567,CL 345, CJ 456,CL 341','CL');
-       assert.equal(fromKuilsriver, 1)
+    var fromStellies = countAllFromTown('CL 124,CY 567,CL 345, CJ 456,CL 341','CL');
+//fromStellies should contains
+assert.equal(fromStellies, 3)
+
+var fromKuilsriver = countAllFromTown('CJ 124,CY 567,CL 345, CF 456, CL 341','CF');
+//fromStellies should contains
+assert.equal(fromKuilsriver, 1)
     });
 
    
--- test/findItemsOver.test.js
+++ test/findItemsOver.test.js
@@ -4,12 +4,35 @@ let findItemsOver =require("../function/findItemsOver");
 describe('findItemsOver' , function(){
 
     it('should return "the products," when it has  quantity higher than the threshold' , function(){
+        var results = [];
+        var itemList = [];
         assert.deepEqual(results, findItemsOver(itemList, 20));
+        
+    });
+    it('should return "the products," when it has  quantity higher than the threshold' , function(){
+        var results2 = [];
+        var itemList2 = [];
         assert.deepEqual(results2, findItemsOver(itemList2, 20));
+        
+    });
+    it('should return "the products," when it has  quantity higher than the threshold' , function(){ 
+        var results3 = [];
+        var itemList3 = [];   
         assert.deepEqual(results3, findItemsOver(itemList3, 20));
-
+            
     });
 
    
 
 }); 
+
+
+
+
+
+
+
+
+
+        
+console.log('findItemsOver passed!');
